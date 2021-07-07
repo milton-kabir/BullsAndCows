a0101ed3895029e4e0698b3b6b7b52fe1db9861d
Milton Kabir <drifter.kabir@gmail.com>
2021-07-08 00:05:16 +0600

    Sixth Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Please, enter the secret code's length:");
+        System.out.println("Input the length of the secret code:");
         Scanner sc = new Scanner(System.in);
         String tt=sc.nextLine();
         int sz = Integer.parseInt(tt);
-        if(sz>10){
+        if(sz>36){
             System.out.println("Error: can't generate a secret number with a length of "+sz+" because there aren't enough unique digits.");
             return;
         }
@@ -26,15 +26,39 @@ public class Main {
         for(int i=0;i<sz;i++){
             st+=ar[i];
         }
-        System.out.println(st);
+//        System.out.println(st);
+        System.out.println("Input the number of possible symbols in the code:");
+        tt=sc.nextLine();
+        int sz1 = Integer.parseInt(tt);
+        if(sz1>36){
+            System.out.println("Error: can't generate a secret number with a length of "+sz+" because there aren't enough unique digits.");
+            return;
+        }
+        int x=sz1-10;
+        if(x<1){
+            x=1;
+        }
+        String ss="abcdefghijklmnopqrstuvwxyz";
+        for(int i=sz;i<x;i++){
+            st+=ss.charAt(i);
+        }
+//        System.out.println(ss.charAt(x-1));
+        String xx="";
+        for(int i=0;i<sz;i++){
+            xx+='*';
+        }
+        System.out.println("The secret is prepared: "+xx+" (0-9, a-"+ss.charAt(x-1)+").");
+
+
         System.out.println("Okay, let's start a game!");
         int cnt=1;
+
         while(true){
             System.out.println("Turn "+cnt+":");
             cnt++;
             String ts=sc.nextLine();
             int b=0,c=0;
-            for(int i=0;i<st.length();i++){
+            for(int i=0;i<ts.length();i++){
                 if(st.charAt(i)==ts.charAt(i)){
                     b++;
                     ts=ts.substring(0,i)+'x'+ts.substring(i+1);
