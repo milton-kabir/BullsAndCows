eb711e639372799ef1463eeecaa28ffb7b42fa2f
Milton Kabir <drifter.kabir@gmail.com>
2021-07-07 23:12:48 +0600

    Fourth Stage Completed

--- src/com/kabir/milton/Main.java
+++ src/com/kabir/milton/Main.java
@@ -1,39 +1,73 @@
 package com.kabir.milton;
 
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        String st=sc.nextLine(),ts="9305";
-        int b=0,c=0;
-        for(int i=0;i<st.length();i++){
-            if(st.charAt(i)==ts.charAt(i)){
-                b++;
-                ts=ts.substring(0,i)+'x'+ts.substring(i+1);
-            }
+        System.out.println("Please, enter the secret code's length:");
+        Scanner sc = new Scanner(System.in);
+        String tt=sc.nextLine();
+        int sz = Integer.parseInt(tt);
+        if(sz>10){
+            System.out.println("Error: can't generate a secret number with a length of "+sz+" because there aren't enough unique digits.");
+            return;
         }
-        for(int i=0;i<st.length();i++){
-            for(int j=0;j<ts.length();j++){
-                if(st.charAt(i)==ts.charAt(j)){
-                    c++;
-                    ts=ts.substring(0,j)+'x'+ts.substring(j+1);
-                    break;
+        int[] ar = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        Random rnd = new Random();
+        for (int i = 0; i < ar.length; i++) {
+            int randomIndexToSwap = rnd.nextInt(ar.length);
+            int temp = ar[randomIndexToSwap];
+            ar[randomIndexToSwap] = ar[i];
+            ar[i] = temp;
+        }
+        String st="";
+        for(int i=0;i<sz;i++){
+            st+=ar[i];
+        }
+        System.out.println(st);
+        System.out.println("Okay, let's start a game!");
+        int cnt=1;
+        while(true){
+            System.out.println("Turn "+cnt+":");
+            cnt++;
+            String ts=sc.nextLine();
+            int b=0,c=0;
+            for(int i=0;i<st.length();i++){
+                if(st.charAt(i)==ts.charAt(i)){
+                    b++;
+                    ts=ts.substring(0,i)+'x'+ts.substring(i+1);
                 }
             }
+            for(int i=0;i<st.length();i++){
+                for(int j=0;j<ts.length();j++){
+                    if(st.charAt(i)==ts.charAt(j)){
+                        c++;
+                        ts=ts.substring(0,j)+'x'+ts.substring(j+1);
+                        break;
+                    }
+                }
+            }
+            if(b==sz){
+                System.out.println("Grade: "+b+" bulls\n" +
+                        "Congratulations! You guessed the secret code.");
+                break;
+            }
+            if(c==0&&b==0){
+                System.out.println("Grade: None.");
+            }
+            else if(c==0&&b!=0){
+                System.out.println("Grade: "+b+" bull(s).");
+            }
+            else if(c!=0&&b==0){
+                System.out.println("Grade: "+c+" cow(s).");
+            }
+            else{
+                System.out.println("Grade: "+b+" bull(s) and "+c+" cow(s).");
+            }
+
         }
-        if(c==0&&b==0){
-            System.out.println("Grade: None. The secret code is 9305.");
-        }
-        else if(c==0&&b!=0){
-            System.out.println("Grade: "+b+" bull(s). The secret code is 9305.");
-        }
-        else if(c!=0&&b==0){
-            System.out.println("Grade: "+c+" cow(s). The secret code is 9305.");
-        }
-        else{
-            System.out.println("Grade: "+b+" bull(s) and "+c+" cow(s). The secret code is 9305.");
-        }
+
     }
 }
